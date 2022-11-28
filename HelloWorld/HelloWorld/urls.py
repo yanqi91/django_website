35ab44b0b3f262ad4db964b45cb74891fc1e4de8
henryqi91 <yanqi@zhejianglab.com>
2022-11-28 17:23:03 +0800

    progress: https://www.runoob.com/django/django-template.html ["if" statement]

--- HelloWorld/HelloWorld/urls.py
+++ HelloWorld/HelloWorld/urls.py
@@ -17,5 +17,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('hello/', views.hello),
+    url('test/', views.test),
 ]
--- HelloWorld/HelloWorld/views.py
+++ HelloWorld/HelloWorld/views.py
@@ -1,4 +1,20 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
-def hello(request):
-    return HttpResponse("Hello world!")
+def test(request):
+
+    views_name = 'Zhejiang Lab'
+    views_list = ['abc','def','ghk']
+    views_dict = {'abc':'zjlab'}
+    views_str = "<a href='https://www.runoob.com/'>点击跳转</a>"
+    views_num = 88
+
+    return render(request,
+                  'test.html',
+                  {
+                      "views_list": views_list,
+                      "views_dict":views_dict,
+                      "name":views_name,
+                      'hype_link':views_str,
+                      'num':views_num
+                   }
+                  )
